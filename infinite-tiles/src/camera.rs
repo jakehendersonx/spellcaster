3d3b5814768d0e8aa8d19fb53059505dc886a54e
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2025-02-17 20:19:24 -0500

    fix: type in position

--- infinite-tiles/src/camera.rs
+++ infinite-tiles/src/camera.rs
@@ -35,7 +35,7 @@ impl Camera {
     }
 
     pub fn world_to_screen(&self, world_pos: Vec2) -> Vec2 {
-        world_pos - self.posiiton
+        world_pos - self.positon
     }
 
     pub fn screen_to_world(&self, screen_pos: Vec2) -> Vec2 {
