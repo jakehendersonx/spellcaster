df17ea68bd292a8af2962a226b468fa2c7163346
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2025-02-17 19:30:38 -0500

    refactor: changed spellcaster-rs to infinite-tiles project

---
+++ infinite-tiles/src/main.rs
@@ -0,0 +1,16 @@
+mod camera;
+mod config;
+mod player;
+mod texture;
+mod types;
+mod world;
+
+use camera::Camera;
+use macroquad::prelude::*;
+use player::Player;
+use world::World;
+
+#[macroquad::main("infinite tiles")]
+async fn main() {
+    //TODO: implement
+}
